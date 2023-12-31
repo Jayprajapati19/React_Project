f73561eecf2653844e34db46a11c8c0f8b2c4602
prajapatijay03 <jayp37679@gmail.com>
2023-12-31 16:59:09 +0530

    new learn

---
+++ 12MegaBlog/src/appwrite/auth.js
@@ -0,0 +1,66 @@
+import conf from '../conf/conf.js';
+import { Client, Account, ID } from "appwrite"
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appWriteUrl)
+            .setProject(conf.appWriteProjectId);
+        this.account = new Account(this.client);
+
+    }
+
+    async createAccount({ email, password, name }) {
+        // eslint-disable-next-line no-useless-catch
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if (userAccount) {
+                // call another method
+                return this.login({email, password});
+
+
+            } else {
+                return userAccount;
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async login({email, password}) {
+        // eslint-disable-next-line no-useless-catch
+        try {
+            return await this.account.createEmailSession(email, password);
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.log("AppWrite service :: getCurrentUser :: error", error);
+        }
+
+        return null;
+    }
+
+    async logout(){
+
+        try {
+            await this.account.deleteSessions();
+        } catch (error) {
+            console.log("AppWrite service :: getCurrentUser :: error", error);
+            
+        }
+    }
+
+}
+
+const authService = new AuthService();
+
+export default authService
---
+++ 12MegaBlog/src/components/index.js
@@ -0,0 +1,16 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from './Container/Container';
+import Logo from './Logo';
+import LogoutBtn from "./Header/LogoutBtn";
+
+
+
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn
+
+}
